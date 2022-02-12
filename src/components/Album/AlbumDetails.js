894c401625d171e7019f022d6132e208afee09cb
aaron <aronkim.cn@gmail.com>
2022-02-13 00:19:45 +0900

    update UI

--- src/components/Album/AlbumDetails.js
+++ src/components/Album/AlbumDetails.js
@@ -5,9 +5,11 @@ import UploadForm from '../Upload/UploadForm';
 import { withRouter } from 'react-router-dom';
 
 //MUI5.0
+import Box from '@mui/material/Box';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
-import { Grid, Paper, Button } from '@mui/material';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+import { Grid, Paper, Button, ButtonGroup } from '@mui/material';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
@@ -21,7 +23,12 @@ class AlbumDetails extends React.Component{
         this.state = {
           imageData: [],
           albumnames: [],
-          open: false
+          lastKey: '',
+          count: 0,
+          open: false,
+          popupUrl: "",
+          page: 1,
+          xsState: 'none'
         }
     }
 
@@ -32,20 +39,68 @@ class AlbumDetails extends React.Component{
         axios.get(rurl,{
             headers: {
                 'Authorization': `token ${this.props.token}`
-            } 
+            }
         }).then(res => {
-            this.setState({ imageData: res.data });
+            console.log("ssssss");
+            console.log(res.data);
+            this.setState({ imageData: res.data.imageData });
+            this.setState({ lastKey: res.data.lastKey });
+            this.setState({ count: res.data.count });
         })
     }
 
-    handleClick = () => {
+    handleClick = (key) => {
         console.log("clicked")
-        this.setState({ open: true })
+        console.log(key)
+        let url =  `${config.API_Endpoint}/pictures/` + this.props.location.state.albumid + "/" + key
+
+        axios.get(url,{
+            headers: {
+                'Authorization': `token ${this.props.token}`
+            } 
+        }).then(res => {
+            console.log("yyyyy");
+            console.log(res.data.img);
+            this.setState({ popupUrl: res.data.img })
+            this.setState({ open: true })
+        })
+    }
+
+    handlePage = (event, value) => {
+
+        this.setState({ page: value})
     }
 
     handleClose = () => {
         console.log("close")
         this.setState({ open: false })
+    }
+
+    nextPage = () => {
+
+        let rurl = `${config.API_Endpoint}/pictures/` + this.props.location.state.albumid + "/thumnail"
+           
+        axios.get(rurl,{
+            headers: {
+                'Authorization': `token ${this.props.token}`
+            },
+            params: {
+                'lastKey': this.state.lastKey
+            },
+        }).then(res => {
+            
+            this.setState({ imageData: res.data.imageData });
+            this.setState({ lastKey: res.data.lastKey });
+            this.setState({ count: res.data.count });
+        })        
+    }
+
+    showform = (currentState) => {
+        if (currentState == 'none'){
+            this.setState({ xsState: 'block'})
+        } else {
+            this.setState({ xsState: 'none'})
+        }
     }
     
     render() {
@@ -55,32 +110,71 @@ class AlbumDetails extends React.Component{
                 <Paper sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around', overflow: 'hidden'}}>
                     {/* <Grid container justify="center" alignItems="stretch" spacing={3}> */}
                         <Grid item xs={12} sm={4}>
-                            <UploadForm albumid={this.props.location.state.albumid} token={this.props.token} />
+                            <Box sx={{
+                                display: { xs: 'block', sm: 'none' },
+                            }}>
+                                <Box sx={{
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    alignItems: 'center',
+                                    '& > *': {
+                                        m: 1,
+                                    },
+                                }}>
+                                    <Button color="secondary" onClick={() => this.showform(this.state.xsState)}>Show/Hide Image Upload Form</Button>
+                                </Box>
+                            </Box>
+                            <Box sx={{ display: { xs: `${this.state.xsState}`, sm: 'block' } }}>
+                                <UploadForm albumid={this.props.location.state.albumid} token={this.props.token} />
+                            </Box>
                         </Grid>
-                        <Grid item xs={12} sm={7}>
-                            <ImageList sx={{ width: "100%", height: 600 }} cols={3} gap={5}>
-                                {this.state.imageData.map((tile) => (
-                                    <ImageListItem key={tile.img} cols={tile.cols || 1} onClick={this.handleClick}>
-                                        <img src={tile.img} alt={tile.title} />
-                                    </ImageListItem>
-                                ))}
-                            </ImageList>
+                        <Grid item xs={12} sm={8}>
+                            <Box sx={{ width: "100%", height: 600, overflowY: 'scroll' }}>
+                                <ImageList variant="masonry" cols={3} gap={5}>
+                                    {this.state.imageData.map((item) => (
+                                        <ImageListItem key={item.img} cols={item.cols || 1} onClick={()=>this.handleClick(item.s3key)}>
+                                            {/* <img src={tile.img} alt={tile.title} /> */}
+                                            <img src={`${item.img}`}
+                                                srcSet={`${item.img}`}
+                                                alt={item.title}
+                                                loading="lazy"></img>
+                                            <ImageListItemBar position="below" title={item.title} />
+                                        </ImageListItem>
+                                    ))}
+                                </ImageList>
+                            </Box>
+                            <Box sx={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                alignItems: 'center',
+                                '& > *': {
+                                    m: 1,
+                                },
+                            }}>
+                                <ButtonGroup variant="text" aria-label="text button group">
+                                    {/* <Button>Previous</Button>    */}
+                                    <Button onClick={this.nextPage}>Next</Button>
+                                </ButtonGroup>
+                            </Box>
                             <Dialog fullScreen ={this.props.isMobile}
                                     open={this.state.open}
                                     onClose={this.handleClose}
                                     aria-labelledby="responsive-dialog-title">
-                            <DialogTitle id="responsive-dialog-title">
-                                    {"Full Image"}
-                            </DialogTitle>
-                            <DialogContent>
-                                <img src="https://www.tv-asahi.co.jp/shinchan/character/img/01.png"></img>
-                            </DialogContent>
-                            <DialogActions>
-                            <Button autoFocus onClick={this.handleClose}>
-                                Close
-                            </Button>
-                            </DialogActions>
+                                <DialogTitle id="responsive-dialog-title">
+                                        {"Full Image"}
+                                </DialogTitle>
+                                <DialogContent>
+                                    <Box sx={{ height:"100%" }}>
+                                        <img src={this.state.popupUrl} width={400} height={"auto"}></img>
+                                    </Box>
+                                </DialogContent>
+                                <DialogActions>
+                                    <Button autoFocus onClick={this.handleClose}>
+                                        Close
+                                    </Button>
+                                </DialogActions>
                             </Dialog>
+
                         </Grid>
                         
                     {/* </Grid> */}
